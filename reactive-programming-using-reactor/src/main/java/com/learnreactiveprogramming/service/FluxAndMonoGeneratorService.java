371761fb613a4d9600a0195dd302e5083f429d6f
Aishwarya <aishwaryas@jfrog.com>
2025-05-25 19:19:15 +0530

    adding mono

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,6 +1,7 @@
 package com.learnreactiveprogramming.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -11,10 +12,18 @@ public class FluxAndMonoGeneratorService {
         return Flux.fromIterable(List.of("Alex", "Ben", "Chloe"));
     }
 
+    public Mono<String> namesMono() {
+        return Mono.just("Alex");
+    }
+
     public static void main(String[] args) {
         FluxAndMonoGeneratorService fmgs = new FluxAndMonoGeneratorService();
 
         fmgs.namesFlux().subscribe(name -> {
+            System.out.println(name);
+        });
+
+        fmgs.namesMono().subscribe(name -> {
             System.out.println(name);
         });
     }
