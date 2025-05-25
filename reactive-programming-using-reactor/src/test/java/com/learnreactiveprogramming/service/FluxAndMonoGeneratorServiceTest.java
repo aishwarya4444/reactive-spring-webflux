faeb6889bd52d55e5a0dcb3306d7c753eff71323
Aishwarya <aishwaryas@jfrog.com>
2025-05-25 23:20:54 +0530

    test for flux

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,42 @@
 package com.learnreactiveprogramming.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
 
 class FluxAndMonoGeneratorServiceTest {
+    FluxAndMonoGeneratorService fmgs = new FluxAndMonoGeneratorService();
 
+    @Test
+    void namesFlux() {
+        //given
+        //when
+        var namesFlux = fmgs.namesFlux();
+        //then
+        StepVerifier.create(namesFlux)
+                .expectNext("Alex", "Ben", "Chloe")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFluxCount() {
+        //given
+        //when
+        var namesFlux = fmgs.namesFlux();
+        //then
+        StepVerifier.create(namesFlux)
+                .expectNextCount(3)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux1Name2Count() {
+        //given
+        //when
+        var namesFlux = fmgs.namesFlux();
+        //then
+        StepVerifier.create(namesFlux)
+                .expectNext("Alex")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
 }
