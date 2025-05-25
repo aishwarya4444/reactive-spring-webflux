d32a72e0ecb6fc154198609d4ac7d90531ee573a
Aishwarya <aishwaryas@jfrog.com>
2025-05-25 19:11:44 +0530

    adding flux

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,21 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+    public Flux<String> namesFlux() {
+        // we will get it from db or a remote service call
+        // for now we pass from here
+        return Flux.fromIterable(List.of("Alex", "Ben", "Chloe"));
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fmgs = new FluxAndMonoGeneratorService();
+
+        fmgs.namesFlux().subscribe(name -> {
+            System.out.println(name);
+        });
+    }
 }
